20d52246ee89c7f8f9ded996fd58e7adfe30e682
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-12-19 17:46:58 +0000

    add API call

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,16 @@
 import styles from './page.module.css';
 import Banner from '../ui/Banner';
 import Card from './_components/Card';
+import pokemon from '../pokemon.json';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Banner buttonText="Search" />
       <section className={styles.cardGrid}>
-        <Card
-          name="Bulbasaur"
-          imgUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          href="/pokemon/1"
-        />
-        <Card
-          name="Charmander"
-          imgUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
-          href="/pokemon/4"
-        />
-        <Card
-          name="Squirtle"
-          imgUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
-          href="/pokemon/7"
-        />
-        <Card
-          name="Pikachu"
-          imgUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
-          href="/pokemon/25"
-        />
+        {pokemon.map((poke) => (
+          <Card key={poke.id} name={poke.name} imgUrl={poke.imgUrl} href={poke.href} />
+        ))}
       </section>
     </div>
   );
--- app/pokemon/[id]/page.tsx
+++ app/pokemon/[id]/page.tsx
@@ -1,11 +1,40 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
-const page = ({ params }: { params: any }) => {
-  console.log(params.id);
+import styles from './pokepage.module.css';
+
+async function getPokemon(id: string) {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
+const page = async ({ params }: { params: any }) => {
+  const pokemon = await getPokemon(params.id);
+  console.log(pokemon);
   return (
     <div>
       <Link href="/">Back to home</Link>
-      <h1>{params.id}</h1>
+      {pokemon && (
+        <section className={styles.pokeSection}>
+          <h1>{pokemon.name}</h1>
+          <div className={styles.imgContainer}>
+            <Image
+              src={pokemon.sprites.other['official-artwork'].front_default}
+              width={176}
+              height={176}
+              alt={pokemon.name || 'pokemon'}
+            />
+          </div>
+        </section>
+      )}
     </div>
   );
 };
